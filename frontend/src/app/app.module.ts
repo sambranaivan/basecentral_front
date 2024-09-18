ca4feeb60bb434aa657bdfb720d7518c99498436
Ivan sambrana <sambranaivan@gmail.com>
2024-09-18 11:08:38 -0300

    configuracion de filtros

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { LegajoComponent } from './legajo/legajo.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
@@ -19,7 +19,9 @@ import { DateRangeFilterComponent } from './date-range-filter/date-range-filter.
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { FilterConfigComponent } from './filter-config/filter-config.component';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -37,6 +39,7 @@ import { FilterConfigComponent } from './filter-config/filter-config.component';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
